c9cdf3ad7f30ea0af8b56b285d40991e86f6ff0b
Fares-Tabet <44642569+Fares-Tabet@users.noreply.github.com>
2020-04-07 10:45:45 -0700

    added source code

---
+++ OSMConnectivity/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OSMConnectivity.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+           
+            ViewBag.json1 = System.IO.File.ReadAllText(Server.MapPath(Url.Content("~/Content/json_files/wayDetails.json")));
+
+            ViewBag.json2 = System.IO.File.ReadAllText(Server.MapPath(Url.Content("~/Content/json_files/trunk.json")));
+
+            ViewBag.json3 = System.IO.File.ReadAllText(Server.MapPath(Url.Content("~/Content/json_files/disconnected.json")));
+
+            return View();
+
+        }
+
+
+    }
+}
---
+++ OSMConnectivity/bin/Release/Publish/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OSMConnectivity.MvcApplication" Language="C#" %>
